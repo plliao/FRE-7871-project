992a1a2e796ec04cf45a5b35921ff59899c9e0ac
Pei-Lun Liao <pll273@nyu.edu>
2018-10-17 14:25:24 -0400

    Process Reuter data

--- process.py
+++ process.py
@@ -1,5 +1,6 @@
 import re
 import json
+import os
 import string
 import pickle
 import datetime
@@ -65,3 +66,63 @@ def read_SNP_dataframe():
         snp[name] = pd.Series([None for _ in range(offset)]).append(snp['Close'][0:-offset] - snp['Open'][0:-offset], ignore_index=True)
     return snp
 
+def find_price(ticker, timestamp):
+    date_str = timestamp.strftime('%Y%m%d')
+    path = 'data/SNP/{0:s}/price/price_{1:s}.json'.format(ticker, date_str)
+
+    if not os.path.exists(path):
+        return None
+
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+
+    start_time = datetime.datetime.strptime(date_str + ' 09:30', '%Y%m%d %H:%M')
+    offset = int((timestamp - start_time).total_seconds() // 60)
+    return data[offset]
+
+def read_reuter_csv():
+    reuter = pd.read_csv('reuter_data.csv')
+    reuter['published_time'] = pd.to_datetime(reuter['published_time'])
+    reuter.sort_values('published_time', inplace=True)
+
+    twenty_min = datetime.timedelta(minutes=20)
+
+    df = pd.DataFrame()
+
+    for _, article in reuter.iterrows():
+        ticker = article['ticker']
+        published_time = article['published_time']
+
+        date_str = published_time.strftime('%Y%m%d')
+        start_time = datetime.datetime.strptime('{0:s} 0930'.format(date_str), '%Y%m%d %H%M')
+        end_time = datetime.datetime.strptime('{0:s} 1600'.format(date_str), '%Y%m%d %H%M')
+
+        predicted_time = published_time + twenty_min
+
+        if published_time > start_time and published_time < end_time and \
+                predicted_time > start_time and predicted_time < end_time:
+            price = find_price(ticker, published_time)
+            predicted_price = find_price(ticker, predicted_time)
+
+            if price is None or predicted_price is None:
+                continue
+
+            datum = pd.Series(
+                data={
+                    'text':clean_sentence(article['text']),
+                    'publish_time':published_time,
+                    'predicted_time':predicted_time,
+                    'price':price['high'],
+                    'predicted_price':price['low'],
+                    'ticker':article['ticker'],
+                    'name':article['name'],
+                    'title':article['title']
+                }
+            )
+            df = df.append(datum, ignore_index=True)
+    df.to_csv('reuter_price.csv', index=False)
+    ipdb.set_trace()
+
+
+if __name__ == '__main__':
+    read_reuter_csv()
