bd93965370d48815c3e51432f5c829bb60843f0a
Pei-Lun Liao <pll273@nyu.edu>
2018-10-23 01:21:51 -0400

    Average word2vec

--- new_model.py
+++ new_model.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 from sklearn.naive_bayes import MultinomialNB
@@ -21,10 +22,16 @@ from sklearn.preprocessing import RobustScaler
 from sklearn import linear_model
 from wordcloud import WordCloud
 from nltk import pos_tag, word_tokenize
+import gensim.downloader as api
 
 MIN_DF = 5
 MAX_DF = 50
 WORD_CLOUD_NUMBER = 50
+
+BOW = "bow"
+TFIDF = "tfidf"
+WORD2VEC = "word2vec"
+SKIPTHOUGHT = "skipThought"
 
 def select_by_pos_tag(sentence, tags):
     word_tokens = word_tokenize(sentence)
@@ -40,22 +47,46 @@ def clean_sentence(s):
     s = re.sub(" +", " ", s)
     return s.strip()
 
-def generate_bag_of_words(news_df, start_index, split_index, end_index, stopwords):
-    all_data = news_df[start_index:end_index]
-    train = news_df[start_index:split_index]
-    test = news_df[split_index:end_index]
-
-    vectorizer = CountVectorizer(stop_words=stopwords, min_df=MIN_DF, max_df=MAX_DF)
+def generate_bag_of_words(train, test, feature_args):
+    vectorizer = CountVectorizer(min_df=MIN_DF, max_df=MAX_DF, **feature_args)
     train_bag_of_words = vectorizer.fit_transform(train['text'].apply(clean_sentence)).toarray()
     test_bag_of_words = vectorizer.transform(test['text'].apply(clean_sentence)).toarray()
 
+    train_bag_of_words = normalize(train_bag_of_words)
+    test_bag_of_words = normalize(test_bag_of_words)
     word_list = vectorizer.get_feature_names()
-    print('word size:', len(word_list))
 
     train_text_df = pd.DataFrame(train_bag_of_words, index=train.index, columns=word_list)
     test_text_df = pd.DataFrame(test_bag_of_words, index=test.index, columns=word_list)
     bag_of_words_df = pd.concat([train_text_df, test_text_df], axis=0)
     return bag_of_words_df, vectorizer
+
+def generate_tfidf(train, test, feature_args):
+    vectorizer = TfidfVectorizer(min_df=MIN_DF, max_df=MAX_DF, **feature_args)
+    train_bag_of_words = vectorizer.fit_transform(train['text'].apply(clean_sentence)).toarray()
+    test_bag_of_words = vectorizer.transform(test['text'].apply(clean_sentence)).toarray()
+
+    word_list = vectorizer.get_feature_names()
+
+    train_text_df = pd.DataFrame(train_bag_of_words, index=train.index, columns=word_list)
+    test_text_df = pd.DataFrame(test_bag_of_words, index=test.index, columns=word_list)
+    bag_of_words_df = pd.concat([train_text_df, test_text_df], axis=0)
+    return bag_of_words_df, vectorizer
+
+def average_word2vec(sentence, model):
+    sentence = clean_sentence(sentence)
+    word2vecs = []
+    for word in sentence.split(" "):
+        word = word.lower()
+        if word in model:
+            word2vecs.append(model[word])
+    return pd.Series(np.average(word2vecs, axis=0))
+
+def generate_word2vec(train, test, feature_args):
+    model = feature_args['model']
+    features = pd.concat([train, test], axis=0)['text'].apply(average_word2vec, model=model)
+    ipdb.set_trace()
+    return features, None
 
 def generate_price_features(data):
     price_feature_name = ['previous_price_{0:d}'.format(d) for d in range(1, 6)]
@@ -83,7 +114,7 @@ def evaluate_return(open_price, y_hat, y):
 
     return revenue, buy_action
 
-def run(data, split, stopwords, exp_label):
+def run(data, split, feature_args, exp_label):
     published_time = pd.to_datetime(data['published_time'])
     y = generate_regression_label(data)
     y_class = generate_classification_label(data)
@@ -109,6 +140,14 @@ def run(data, split, stopwords, exp_label):
         }
     }
 
+    feature_list = [BOW, TFIDF, WORD2VEC, SKIPTHOUGHT]
+    feature_functions = {
+        BOW:generate_bag_of_words,
+        TFIDF:generate_tfidf,
+        WORD2VEC:generate_word2vec,
+        SKIPTHOUGHT:None
+    }
+
     fold_index = 0
     tscv = TimeSeriesSplit(n_splits=split)
     for train_index, test_index in tscv.split(data.values):
@@ -117,13 +156,25 @@ def run(data, split, stopwords, exp_label):
         start_index = data.index[train_index[0]]
         split_index = data.index[test_index[0]]
         end_index = data.index[test_index[-1]] + 1
+        train = data[start_index:split_index]
+        test = data[split_index:end_index]
 
-        bag_of_words, vectorizer = generate_bag_of_words(data, start_index, split_index, end_index, stopwords)
-        X = bag_of_words.values
+        X_list = []
+        for feature_name in feature_list:
+            if feature_name in feature_args:
+                features, vectorizer = feature_functions[feature_name](train, test, feature_args[feature_name])
+                X_list.append(features)
 
-        word_size = X.shape[1]
+        if len(X_list) > 1:
+            array_list = [features.values for features in X_list]
+            X = np.concatenate(array_list, axis=1)
+        else:
+            X = X_list[0].values
+
+        feature_size = X.shape[1]
+        print("feature size:", feature_size)
+
         X_train, X_test = X[train_index], X[test_index]
-
         y_train, y_test = y[train_index], y[test_index]
         y_class_train, y_class_test = y_class[train_index], y_class[test_index]
 
@@ -135,8 +186,8 @@ def run(data, split, stopwords, exp_label):
         scaler.fit(y_train.reshape(-1, 1))
         y_train_t = scaler.transform(y_train.reshape(-1, 1)).reshape(-1, )
 
-        x_train_t = normalize(X_train)
-        x_test_t = normalize(X_test)
+        x_train_t = X_train
+        x_test_t = X_test
 
         # Modeling
         classifiers_dict = {
@@ -317,17 +368,18 @@ def plot_record(record, label, selected_tasks):
                 ax.set(xlabel='Fold number', ylabel=measure_map[task])
                 plt.savefig(path)
 
-def do_exp(data, num_split, stopwords, data_label, feature_label):
-    if stopwords is None:
-        exp_label = "with stopwords ({0:s}, {1:s})".format(data_label, feature_label)
+def do_exp(data, num_split, feature_args, data_label, feature_label):
+    if feature_args['stop_words'] is None:
+        exp_label = "with stop_words ({0:s}, {1:s})".format(data_label, feature_label)
     else:
-        exp_label = "without stopwords ({0:s}, {1:s})".format(data_label, feature_label)
+        exp_label = "without stop_words ({0:s}, {1:s})".format(data_label, feature_label)
     print(exp_label)
+    print(feature_args)
 
     selected_tasks = ['classification', 'regression', 'pnl']
 
     file_name_label = re.sub(' +', '_', exp_label)
-    record = run(data, num_split, stopwords, exp_label)
+    record = run(data, num_split, feature_args, exp_label)
     #plot_record(record, exp_label, selected_tasks)
     #pickle.dump(record, open('result/record_{0:s}.p'.format(file_name_label), 'wb'))
     return record
@@ -342,7 +394,7 @@ def plot_records(records, model):
         for feature_label, record in records.items():
             if model in record[task]['train']:
                 train_record_list.append(record[task]['train'][model].rename(feature_label))
-                test_record_list.append(record[task]['train'][model].rename(feature_label))
+                test_record_list.append(record[task]['test'][model].rename(feature_label))
 
         if len(train_record_list) > 0:
             merged_record[task] = {}
@@ -370,29 +422,82 @@ def main():
 
     num_split = 5
 
+    feature_label = "BOW stop_words"
+    feature_param = {
+        'stop_words':None,
+        BOW:{}
+    }
+    records[feature_label] = do_exp(news_df, num_split, feature_param, data_label, feature_label)
+
     feature_label = "BOW"
-    records['stopwords'] = do_exp(news_df, num_split, None, data_label, feature_label)
-    records[feature_label] = do_exp(news_df, num_split, "english", data_label, feature_label)
+    feature_param['stop_words'] = "english"
+    feature_param[BOW]['stop_words'] = "english"
+    records[feature_label] = do_exp(news_df, num_split, feature_param, data_label, feature_label)
+    '''
+    feature_label = "BOW tri-gram"
+    feature_param[BOW]['ngram_range'] = (1, 3)
+    records[feature_label] = do_exp(news_df, num_split, feature_param, data_label, feature_label)
 
     tags = ['NNP', 'NNPS']
     feature_label = "BOW proper noun"
+    del feature_param[BOW]['ngram_range']
     news_df['text'] = raw_text.apply(select_by_pos_tag, tags=tags)
-    records[feature_label] = do_exp(news_df, num_split, "english", data_label, feature_label)
+    records[feature_label] = do_exp(news_df, num_split, feature_param, data_label, feature_label)
 
+    '''
     tags = ['NNP', 'NNPS', 'NN', 'NNS']
     feature_label = "BOW noun"
     news_df['text'] = raw_text.apply(select_by_pos_tag, tags=tags)
-    records[feature_label] = do_exp(news_df, num_split, "english", data_label, feature_label)
+    records[feature_label] = do_exp(news_df, num_split, feature_param, data_label, feature_label)
 
+    '''
     tags = ['NNP', 'NNPS', 'NN', 'NNS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
     feature_label = "BOW noun and verb"
     news_df['text'] = raw_text.apply(select_by_pos_tag, tags=tags)
-    records[feature_label] = do_exp(news_df, num_split, "english", data_label, feature_label)
+    records[feature_label] = do_exp(news_df, num_split, feature_param, data_label, feature_label)
 
     tags = ['NNP', 'NNPS', 'NN', 'NNS', 'JJ', 'JJR', 'JJS']
     feature_label = "BOW noun and adj"
     news_df['text'] = raw_text.apply(select_by_pos_tag, tags=tags)
-    records[feature_label] = do_exp(news_df, num_split, "english", data_label, feature_label)
+    records[feature_label] = do_exp(news_df, num_split, feature_param, data_label, feature_label)
+
+    tags = ['NNP', 'NNPS', 'NN', 'NNS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'JJ', 'JJR', 'JJS']
+    feature_label = "BOW noun, verb and adj"
+    news_df['text'] = raw_text.apply(select_by_pos_tag, tags=tags)
+    records[feature_label] = do_exp(news_df, num_split, feature_param, data_label, feature_label)
+
+    feature_label = "BOW and TFIDF"
+    feature_param[TFIDF] = {
+        'stop_words':"english"
+    }
+    records[feature_label] = do_exp(news_df, num_split, feature_param, data_label, feature_label)
+
+    tags = ['NNP', 'NNPS', 'NN', 'NNS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+    feature_label = "BOW, TFIDF noun and verb"
+    news_df['text'] = raw_text.apply(select_by_pos_tag, tags=tags)
+    records[feature_label] = do_exp(news_df, num_split, feature_param, data_label, feature_label)
+    '''
+
+    feature_label = "TFIDF"
+    del feature_param[BOW]
+    feature_param[TFIDF] = {
+        'stop_words':"english"
+    }
+    records[feature_label] = do_exp(news_df, num_split, feature_param, data_label, feature_label)
+
+    '''
+    tags = ['NNP', 'NNPS', 'NN', 'NNS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+    feature_label = "TFIDF noun and verb"
+    news_df['text'] = raw_text.apply(select_by_pos_tag, tags=tags)
+    records[feature_label] = do_exp(news_df, num_split, feature_param, data_label, feature_label)
+    '''
+
+    feature_label = "word2vec"
+    del feature_param[TFIDF]
+    feature_param[WORD2VEC] = {
+        'model':api.load("word2vec-google-news-300")
+    }
+    records[feature_label] = do_exp(news_df, num_split, feature_param, data_label, feature_label)
 
     plot_records(records, 'Ridge Regression')
     #plot_records(records, 'Logistic Regression')
